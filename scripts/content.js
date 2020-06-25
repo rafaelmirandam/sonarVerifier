31e3b8ed14592a951205f32c47849225507f5352
Rafael Miranda <rafael.miranda@capco.com>
2020-06-25 13:29:21 -0300

    organizing

--- scripts/content.js
+++ scripts/content.js
@@ -1,23 +1,23 @@
-var url = window.location.pathname;
+var urlWithProject = window.location.pathname;
 var fromBranch = document.getElementsByClassName('branch-name')[0].innerText;
 var toBranch = document.getElementsByClassName('branch-name')[1].innerText;
-var auth = 'Basic ' + btoa("m190498@corpr.bradesco.com.br:Cap@0805");
+var auth = 'Basic bTE5MDQ5OEBjb3Jwci5icmFkZXNjby5jb20uYnI6Q2FwQDA4MDU=';
 var issues = [];
 
-var project = getProject(url);
-console.log(project);
+var project = getProject(urlWithProject);
 
 callSonar(fromBranch);
 callSonar(toBranch);
-var result = issues[0]-issues[1];
-console.log(issues[0]+"-"+issues[1]+"="+result);
 
-insertTag(result);
+insertTag(issues[0]-issues[1]);
 
 
 function callSonar(branch) {
+    var url = new URL("http://192.168.45.73:9000/api/issues/search");
+    url.searchParams.set("componentKeys", project+":"+branch);
+    url.searchParams.set("statuses", "OPEN");
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://192.168.45.73:9000/api/issues/search?componentKeys="+project+":"+branch, false);
+    xhr.open("GET", url, false);
     xhr.setRequestHeader('Authorization', auth);
     xhr.setRequestHeader('Content-Type','application/json')
     xhr.setRequestHeader('dataType', 'jsonp')
@@ -47,14 +47,16 @@ function insertTag(issueNumber) {
     a.setAttribute('target', '_blank');
     a.className = "build-status-overview-link";
     var spanIcon = document.createElement("SPAN");
+    var text
     if (issueNumber == 0){
         spanIcon.className = approve;
+        text = document.createTextNode(issueNumber+" sonarqube issue(s), but you can take a look anyway")
     } else {
         spanIcon.className = reprove;
         a.setAttribute("style", "color:#E53C17");
+        text = document.createTextNode(issueNumber+" sonarqube issue(s)");
     }
     var span = document.createElement("SPAN");
-    var text = document.createTextNode(issueNumber+" sonarqube issue(s)")
     span.appendChild(text);
     a.appendChild(spanIcon);
     a.appendChild(span);
