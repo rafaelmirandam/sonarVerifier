189283ce4e2aa5fb50f648871abee7824384d180
Rafael Miranda <rafael.miranda@capco.com>
2020-06-25 14:46:03 -0300

    correct filter OK

--- scripts/content.js
+++ scripts/content.js
@@ -1,21 +1,36 @@
 var urlWithProject = window.location.pathname;
-var fromBranch = document.getElementsByClassName('branch-name')[0].innerText;
-var toBranch = document.getElementsByClassName('branch-name')[1].innerText;
+var branch = document.getElementsByClassName('branch-name')[0].innerText;
 var auth = 'Basic bTE5MDQ5OEBjb3Jwci5icmFkZXNjby5jb20uYnI6Q2FwQDA4MDU=';
-var issues = [];
+var firstAnalisysDate = '';
 
 var project = getProject(urlWithProject);
 
-callSonar(fromBranch);
-callSonar(toBranch);
+findFirstAnalisys(branch)
+findIssues(branch, firstAnalisysDate);
 
-insertTag(issues[0]-issues[1]);
+function findFirstAnalisys(branch) {
+    var url = new URL("http://192.168.45.73:9000/api/project_analyses/search");
+    url.searchParams.set("project", project+":"+branch);
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, false);
+    xhr.setRequestHeader('Authorization', auth);
+    xhr.setRequestHeader('Content-Type','application/json')
+    xhr.setRequestHeader('dataType', 'jsonp')
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) { 
+            var response = JSON.parse(xhr.responseText);
+            firstAnalisysDate = response.analyses[response.analyses.length -1].date;
+            firstAnalisysDate = firstAnalisysDate.substring(0, 10);
+        }
+    }
+    xhr.send();
+}
 
-
-function callSonar(branch) {
+function findIssues(branch, firstAnalisysDate) {
     var url = new URL("http://192.168.45.73:9000/api/issues/search");
     url.searchParams.set("componentKeys", project+":"+branch);
     url.searchParams.set("statuses", "OPEN");
+    url.searchParams.set("createdAfter", firstAnalisysDate)
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url, false);
     xhr.setRequestHeader('Authorization', auth);
@@ -23,7 +38,7 @@ function callSonar(branch) {
     xhr.setRequestHeader('dataType', 'jsonp')
     xhr.onreadystatechange = function() {
         if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            issues.push(JSON.parse(xhr.responseText).total);
+            insertTag(JSON.parse(xhr.responseText).total);
         }
     }
     xhr.send();
@@ -43,14 +58,14 @@ function insertTag(issueNumber) {
     var div = document.createElement("DIV");
     div.className = "plugin-item build-status-summary";
     var a = document.createElement("A");
-    a.setAttribute("href","http://192.168.45.73:9000/dashboard?id="+project+":"+fromBranch);
+    a.setAttribute("href","http://192.168.45.73:9000/dashboard?id="+project+":"+branch);
     a.setAttribute('target', '_blank');
     a.className = "build-status-overview-link";
     var spanIcon = document.createElement("SPAN");
     var text
     if (issueNumber == 0){
         spanIcon.className = approve;
-        text = document.createTextNode(issueNumber+" sonarqube issue(s), but you can take a look anyway")
+        text = document.createTextNode("No issues on sonar, have a cookie :)")
     } else {
         spanIcon.className = reprove;
         a.setAttribute("style", "color:#E53C17");
